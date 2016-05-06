e40dc48f3961b4611f4e6a0d3aeb1cb8dc3764b6
James Reece <jamesreece1496@gmail.com>
2016-05-06 20:29:41 +1000

    First commit

---
+++ Assets/DisableVR.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+
+public class DisableVR : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void disableVR(){
+		
+		VRSettings.enabled = false;
+	}
+
+	public void enableVR(){
+
+		VRSettings.enabled = true;
+	}
+}
